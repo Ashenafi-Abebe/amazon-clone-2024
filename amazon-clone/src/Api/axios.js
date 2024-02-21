ecf18a6c8d67665361d055c18f23c9f4c4da4a35
Ashemarc <152915640+Ashenafi-Abebe@users.noreply.github.com>
2024-02-20 18:38:31 -0800

    thunder clint

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-20305/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-20305/us-central1/api",
+
+  baseURL: "https://api-gxsf25hz3a-uc.a.run.app",
 });
 
 export { axiosInstance };
