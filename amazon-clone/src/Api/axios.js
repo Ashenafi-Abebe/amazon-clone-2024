e55abcb612ce8b826705fc911d0b23a20d5313f5
Ashemarc <152915640+Ashenafi-Abebe@users.noreply.github.com>
2024-02-27 09:58:05 -0800

    deploying to netlify

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-20305/us-central1/api",
 
-  baseURL: "https://api-gxsf25hz3a-uc.a.run.app",
+  baseURL: " https://api-gxsf25hz3a-uc.a.run.app",
 });
 
 export { axiosInstance };
